b0f4d33f7d1c82044f67ee7f50475c7404b26a24
Mik Pedersen <45688364+MikPedersen@users.noreply.github.com>
2020-01-06 11:28:02 +0100

    Initial commit

---
+++ src/main/java/demo/WhackApp.java
@@ -0,0 +1,15 @@
+package demo;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.settings.GameSettings;
+
+public  class WhackApp extends GameApplication {
+    @Override
+    protected void initSettings(GameSettings gameSettings) {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
